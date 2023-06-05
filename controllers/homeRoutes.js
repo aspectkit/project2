a2d057e990834e779fda6714cb5f3b4a177c40f5
kit <dhinkit@gmail.com>
2023-06-04 21:57:19 -0600

    fixed parse error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,19 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const {User} = require("../models");
+
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('gameLib', {
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
+
+
+
 
 module.exports = router;
