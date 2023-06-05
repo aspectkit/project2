b39195cfff8054e9057e94b8b7ee3b642bd6f51e
kit <dhinkit@gmail.com>
2023-06-04 20:54:33 -0600

    added router exports

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const path = require('path');
-const routes = require('./routes');
-const sequelize = require('./config/connection')
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,8 +24,8 @@ const sess = {
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize
-    })
+    db: sequelize
+})
 };
 
 app.use(session(sess));
@@ -39,8 +39,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-
-
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+    app.listen(PORT, () => console.log('Now listening'));
 });
