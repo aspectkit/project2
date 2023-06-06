b879fb2b1adf84088e11908b3e7c75de87037a47
kit <dhinkit@gmail.com>
2023-06-05 19:59:28 -0600

    added rawg functionality to get data

--- public/js/addgame.js
+++ public/js/addgame.js
@@ -1,3 +1,71 @@
 // ex: GET https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added
 
 // ex: GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
+
+// require('dotenv').config();
+let gameData;
+const gameSearchHandler = async (event) => {
+    event.preventDefault();
+    console.log("game search button clicked!");
+
+    let game = document.querySelector('#gameSearch').value.trim(); // super mario galaxy
+    game = game.split(" ").join("-").toLowerCase();  // super-mario-galaxy
+    const url = `https://api.rawg.io/api/games/${game}?key=1b296d98b4fd4a98bdcb614862605bfc`; // https://api.rawg.io/api/games/super-mario-galaxy?key=
+    let gameCards = document.querySelector('.games');
+    if (game){ // if exists fetch the data from the url 
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json' },
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            // console.log(data.name);
+            // console.log(data.metacritic);
+            // console.log(data.released);
+            // console.log(data.platforms[0].platform.name);
+            // console.log(data.background_image);
+            const firstDiv = document.createElement('div');
+            firstDiv.classList.add('card');
+            firstDiv.style.width = '18rem';
+
+            const gameImg = document.createElement('img');
+            gameImg.classList.add('card-img-top');
+            gameImg.setAttribute('src', data.background_image);
+            gameImg.setAttribute('alt', "background image of game");
+
+            const secondDiv = document.createElement('div');
+            secondDiv.classList.add('card-body');
+
+            const h5 = document.createElement('h5');
+            h5.classList.add('card-title');
+            h5.textContent = data.name;
+
+            const p1 = document.createElement('p');
+            p1.classList.add('card-text');
+            p1.textContent = `Metacritic score: ${data.metacritic}`;
+
+            const p2 = document.createElement('p');
+            p2.classList.add('card-text');
+            p2.textContent = `Console: ${data.platforms[0].platform.name}`;
+
+            const p3 = document.createElement('p');
+            p3.classList.add('card-text');
+            p3.textContent = `Release Date: ${data.released}`;
+
+            firstDiv.appendChild(gameImg);
+            secondDiv.appendChild(h5);
+            secondDiv.appendChild(p1);
+            secondDiv.appendChild(p2);
+            secondDiv.appendChild(p3);
+            firstDiv.appendChild(secondDiv);
+            
+            
+            gameCards.appendChild(firstDiv);
+        })
+    }
+}
+
+
+
+document.querySelector('#gameSearchButton').addEventListener('click', gameSearchHandler);
