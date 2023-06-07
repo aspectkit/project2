d1a0d325141d21b00f997a0bbe686091697b7b4f
kit <dhinkit@gmail.com>
2023-06-06 18:00:47 -0600

    added some changes to how the cards look and appear and added websites

--- public/js/addgame.js
+++ public/js/addgame.js
@@ -13,6 +13,8 @@ let gameScore;
 let gameDate;
 let gamePlatform;
 let gameBkImg;
+let gameWebsite;
+let gameFound = true;
 
     if (game){ // if exists fetch the data from the url 
         const response = await fetch(url, {
@@ -21,71 +23,95 @@ let gameBkImg;
         }).then(response => {
             return response.json();
         }).then(data => {
-            console.log(data);
-            const title = data.name;
-            const metacritic = data.metacritic;
-            const releaseDate = data.released;
-            const platform = data.platforms[0].platform.name;
-            const backgroundImg = data.background_image;
+            if (data.detail != "Not found."){
+                gameFound = true;
+                console.log(data);
 
-            const firstDiv = document.createElement('div');
-            firstDiv.classList.add('card', 'text-dark', 'm-1', 'text-center');
-            firstDiv.style.width = '18rem';
+                const title = data.name;
+                const metacritic = data.metacritic;
+                const releaseDate = data.released;
+                const platform = data.platforms[0].platform.name;
+                const backgroundImg = data.background_image;
+                const website = data.website;
 
-            const gameImg = document.createElement('img');
-            gameImg.classList.add('card-img-top');
-            gameImg.setAttribute('src', data.background_image);
-            gameImg.setAttribute('alt', "background image of game");
+                const platformsArr = [];
+                for (let i = 0; i < data.platforms.length; i++){
+                    platformsArr.push(data.platforms[i].platform.name);
+                }
 
-            const secondDiv = document.createElement('div');
-            secondDiv.classList.add('card-body');
+                const firstDiv = document.createElement('div');
+                firstDiv.classList.add('card', 'text-dark', 'm-1', 'text-center');
+                firstDiv.style.width = '18rem';
 
-            const h5 = document.createElement('h5');
-            h5.classList.add('card-title');
-            h5.textContent = data.name;
+                const gameImg = document.createElement('img');
+                gameImg.classList.add('card-img-top');
+                gameImg.setAttribute('src', data.background_image);
+                gameImg.setAttribute('alt', "background image of game");
 
-            const p1 = document.createElement('p');
-            p1.classList.add('card-text');
-            p1.textContent = `Metacritic score: ${data.metacritic}`;
+                const webTag = document.createElement('a');
+                webTag.setAttribute('href', data.website);
+                webTag.setAttribute('target', '_blank');
 
-            const p2 = document.createElement('p');
-            p2.classList.add('card-text');
-            p2.textContent = `Console: ${data.platforms[0].platform.name}`;
+                const secondDiv = document.createElement('div');
+                secondDiv.classList.add('card-body');
 
-            const p3 = document.createElement('p');
-            p3.classList.add('card-text');
-            p3.textContent = `Release Date: ${data.released}`;
+                const h5 = document.createElement('h5');
+                h5.classList.add('card-title');
+                h5.textContent = data.name;
 
-            firstDiv.appendChild(gameImg);
-            secondDiv.appendChild(h5);
-            secondDiv.appendChild(p1);
-            secondDiv.appendChild(p2);
-            secondDiv.appendChild(p3);
-            firstDiv.appendChild(secondDiv);
+                const p1 = document.createElement('p');
+                p1.classList.add('card-text');
+                p1.textContent = `Metacritic score: ${data.metacritic}`;
+
+                const p2 = document.createElement('p');
+                p2.classList.add('card-text');
+                p2.textContent = `Consoles: ${platformsArr.join(', ')}`;
+
+                const p3 = document.createElement('p');
+                p3.classList.add('card-text');
+                p3.textContent = `Release Date: ${data.released}`;
+
+                if (data.website != ""){
+                    webTag.appendChild(gameImg);
+                    firstDiv.appendChild(webTag);
+                } else {
+                    firstDiv.appendChild(gameImg);
+                }
             
-            
-            gameCards.appendChild(firstDiv);
+                secondDiv.appendChild(h5);
+                secondDiv.appendChild(p1);
+                secondDiv.appendChild(p2);
+                secondDiv.appendChild(p3);
+                firstDiv.appendChild(secondDiv);
+                
+                
+                gameCards.appendChild(firstDiv);
 
-            gameName = title;
-            gameScore = metacritic;
-            gameDate = releaseDate;
-            gamePlatform = platform;
-            gameBkImg = backgroundImg;
-
-            
+                gameName = title;
+                gameScore = metacritic;
+                gameDate = releaseDate;
+                gamePlatform = platform;
+                gameBkImg = backgroundImg;
+                gameWebsite = website;
+                } else {
+                    gameFound = false;
+                    alert("Game not found!");
+                }
         });
 
-        const response2 = await fetch('/api/games/', {
-            method: 'POST',
-            body: JSON.stringify({gameName, gameScore, gamePlatform, gameDate, gameBkImg }),
-            headers: {'Content-Type': 'application/json'}
-        });
-        if (response2.ok){
-            alert("game saved!")
-        } else {
-            alert("save game failed!");
-        } 
-    }
+        if (gameFound){
+            const response2 = await fetch('/api/games/', {
+                method: 'POST',
+                body: JSON.stringify({gameName, gameScore, gamePlatform, gameDate, gameBkImg }),
+                headers: {'Content-Type': 'application/json'}
+            });
+            if (response2.ok){
+                alert("game saved!")
+            } else {
+                alert("save game failed!");
+            } 
+        }
+    } 
 }
 
 
