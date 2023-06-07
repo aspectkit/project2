bae62623507f2bb2998446cab70e2c057c57b729
Kyle Lucas <epipkl@gmail.com>
2023-06-06 18:56:38 -0600

    updated stylings

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
+
+
 
 const sess = {
     secret: 'Super secret secret',
